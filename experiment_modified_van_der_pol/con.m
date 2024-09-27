8b40f1d0fc5bf8da40b7c88603bfb935f0eafe19
eliasniepoetter <eliasniepoetter@gmail.com>
2024-09-27 12:55:19 +0200

    removed redundant experiment

--- experiment_modified_van_der_pol/con.m
+++
@@ -1,4 +0,0 @@
-function [c,ceq] = con(x,x_i,search_range)
-    c = double((x(1)-x_i(1))^2 + (x(2)-x_i(2))^2 - search_range^2);
-    ceq = [];
-end
--- experiment_modified_van_der_pol/controller_synthesis.m
+++
@@ -1,7 +0,0 @@
-function K = controller_synthesis(A,B)
-
-Q = eye(2).*10;
-R = 1;
-K = lqr(A,B,Q,R);
-
-end
